6e0fe9150349822a7939a1ec35dbfa7a0d659d6c
vijaykrishnad <vijaykrishnad@gmail.com>
2019-09-22 20:12:34 +0530

    first commit

---
+++ src/main/java/Mygrp/das/App.java
@@ -0,0 +1,13 @@
+package Mygrp.das;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
